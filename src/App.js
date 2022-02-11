68ef3b9a2b38bfad94fd88f24ab9326948529020
ictus <ictus@ictusui-MacBookPro.local>
2022-02-11 11:32:17 +0900

    Pseudo Selector and Animations

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 
-const Input = styled.input.attrs({required: true, minLength: 10})`
-background-color: teal;
+const Wrapper = styled.div`
+  display: flex;
+`
+
+const rolling = keyframes`
+  0% {
+    transform: rotate(0deg)
+  }
+  50% {
+    transform: rotate(180deg);
+    border-radius: 50%;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0;
+  }
+`
+
+const Emoji = styled.span`
+  font-size:100px;
+`
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: teal;
+  animation: ${rolling} 1s linear infinite;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${Emoji} {
+    font-size: 100px;
+
+    &:hover {
+      font-size: 50px;
+    }
+  }
 `
 
 function App() {
     return (
         <div className="App">
-            <Input />
-            <Input />
-            <Input />
-            <Input />
-            <Input />
+            <Wrapper>
+                <Box>
+                    <Emoji>😍</Emoji>
+                </Box>
+                <Emoji>😍</Emoji>
+            </Wrapper>
         </div>
     );
 }
