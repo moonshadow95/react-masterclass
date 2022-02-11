09411cedacabf769b971aa73e40c5e37ea659887
ictus <ictus@ictusui-MacBookPro.local>
2022-02-11 10:23:55 +0900

    My first Styled Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import styled from 'styled-components'
+
+const Father = styled.div`
+  display: flex
+`
+const Box = styled.div`
+background-color: ${props=>props.bgColor};
+  width: 100px;
+  height: 100px;
+`
+
 function App() {
     return (
         <div className="App">
-            <h1>Hi</h1>
+            <Father>
+                <Box bgColor="teal"/>
+                <Box bgColor="tomato"/>
+            </Father>
         </div>
     );
 }
