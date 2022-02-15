a85bf33c64fc7f3553cc0caf90478b2d7c9eedb9
ictus <ictus@ictusui-MacBookPro.local>
2022-02-15 12:27:05 +0900

    Nested Routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Coins from "./Coins";
 import Coin from "./Coin";
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Router = () => (
     <BrowserRouter>
         <Routes>
-            <Route path='/:coinId' element={<Coin/>}/>
+            <Route path='/:coinId' element={<Coin/>}>
+                <Route path='chart' element={<Chart/>}/>
+                <Route path='price' element={<Price/>}/>
+            </Route>
             <Route path='/' element={<Coins/>}/>
         </Routes>
     </BrowserRouter>
