acc12a9bfdbbf4b9044a155d639cd4be18319bb7
ictus <ictus@ictusui-MacBookPro.local>
2022-02-15 10:48:47 +0900

    Data Types

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -35,11 +35,73 @@ interface RouteState {
     }
 }
 
+interface InfoData {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+    contract: string,
+    platform: string,
+    contracts: object,
+    parent: object,
+    description: string,
+    message: string,
+    open_source: boolean,
+    started_at: string,
+    development_status: string,
+    hardware_wallet: boolean,
+    proof_type: string,
+    org_structure: string,
+    hash_algorithm: string,
+    links: object,
+    links_extended: object,
+    whitepaper: object,
+    first_data_at: string,
+    last_data_at: string,
+}
+
+interface PriceData {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    circulating_supply: number,
+    total_supply: number,
+    max_supply: number,
+    beta_value: number,
+    first_data_at: string,
+    last_updated: string,
+    quotes: {
+        USD: {
+            ath_date: string,
+            ath_price: number,
+            market_cap: number,
+            market_cap_change_24h: number,
+            percent_change_1h: number,
+            percent_change_1y: number,
+            percent_change_6h: number,
+            percent_change_7d: number,
+            percent_change_12h: number,
+            percent_change_15m: number,
+            percent_change_24h: number,
+            percent_change_30d: number,
+            percent_change_30m: number,
+            percent_from_price_ath: number,
+            price: number,
+            volume_24h: number,
+            volume_24h_change_24h: number,
+        }
+    }
+}
+
 
 const Coin = () => {
     const [loading, setLoading] = useState(true)
-    const [info, setInfo] = useState({})
-    const [price, setPrice] = useState({})
+    const [info, setInfo] = useState<InfoData>()
+    const [priceInfo, setPriceInfo] = useState<PriceData>()
     const {coinId} = useParams() as RouteParams
     const {state} = useLocation() as RouteState
 
@@ -52,7 +114,8 @@ const Coin = () => {
                 await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
             ).json()
             setInfo(infoData)
-            setPrice(priceData)
+            setPriceInfo(priceData)
+            setLoading(false)
         })()
     }, []);
     return (
@@ -61,7 +124,7 @@ const Coin = () => {
                 <Title>{state?.name || "Loading..."}</Title>
             </Header>
             {loading ?
-                <Loader>Loading...</Loader> : null
+                <Loader>Loading...</Loader> : priceInfo?.quotes.USD.market_cap
             }
         </Container>
     )
