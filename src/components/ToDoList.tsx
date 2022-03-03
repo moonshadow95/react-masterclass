97081defe4a919150bb131354b3faa1d5342e8e6
ictus <ictus@ictusui-MacBookPro.local>
2022-03-03 15:56:23 +0900

    Add To Do

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,15 +1,39 @@
 import React, {useState} from 'react'
 import {useForm} from 'react-hook-form'
+import {atom, useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
 
 interface IForm {
     toDo: string
 }
 
+interface IToDo {
+    id: number
+    text: string
+    category: "DONE" | "DOING" | "TO_DO"
+}
+
+const toDoState = atom<IToDo[]>({
+    key: 'toDo',
+    default: []
+})
+
 const ToDoList = () => {
+    // atom으로부터 값 불러오기
+    // const value = useRecoilValue(toDoState)
+    // 불러온 값 변경하기
+    // const modFn = useSetRecoilState(toDoState)
+
+    // 한줄로 사용하기 setState와 닮았다.
+    const [toDos, setToDos] = useRecoilState(toDoState)
     const {register, handleSubmit, formState: {errors}, setValue} = useForm<IForm>()
-    const handleValid = (data: IForm) => setValue('toDo', "")
+    const handleValid = ({toDo}: IForm) => {
+        setToDos((oldToDos) => [{id: Date.now(), text: toDo, category: "TO_DO"}, ...oldToDos])
+        setValue('toDo', "")
+    }
     return (
         <div>
+            <h1>To Dos</h1>
+            <hr/>
             <form onSubmit={handleSubmit(handleValid)}>
                 <input {...register('toDo', {
                     required: "Write a to do!"
@@ -17,6 +41,9 @@ const ToDoList = () => {
                 <span>{errors?.toDo?.message}</span>
                 <button>Add</button>
             </form>
+            <ul>
+                {toDos.map(toDo => <li key={toDo.id}>{toDo.text}</li>)}
+            </ul>
         </div>
     )
 };
