6e84ea639bf95ab951b7ab36b74e48c9ef5697f8
ictus <ictus@ictusui-MacBookPro.local>
2022-03-04 12:04:34 +0900

    Immutability

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import {IToDo} from "../atoms";
+import {IToDo, toDoState} from "../atoms";
+import {useSetRecoilState} from "recoil";
 
 const ToDo = ({id, text, category}: IToDo) => {
+    const setToDos = useSetRecoilState(toDoState)
     const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         const {currentTarget: {name}} = event
+        setToDos(prevDoTos => {
+            const targetIndex = prevDoTos.findIndex(todo => todo.id === id)
+            const newToDo = {id, text, category: name as IToDo['category']}
+            const newToDos = [...prevDoTos]
+            newToDos.splice(targetIndex, 1, newToDo)
+            return newToDos
+        })
     }
     return (
         <li>
