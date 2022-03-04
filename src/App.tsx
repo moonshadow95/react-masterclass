4bd474cdf6e425f4b3eed8b8c44c6cd3452f2e1a
ictus <ictus@ictusui-MacBookPro.local>
2022-03-04 17:33:01 +0900

    Set Selectors

--- src/App.tsx
+++ src/App.tsx
@@ -69,16 +69,19 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
     const [minutes, setMinutes] = useRecoilState(minuteState)
-    const hours = useRecoilValue(hourSelector)
-    const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const [hours, setHours] = useRecoilState(hourSelector)
+    const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
         setMinutes(+event.currentTarget.value)
+    }
+    const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+        setHours(+event.currentTarget.value)
     }
     return (
         <>
             <GlobalStyle/>
             <div>
-                <input type="number" placeholder="Minutes" onChange={onMinuteChange} value={minutes}/>
-                <input type="number" placeholder="Hours" value={hours}/>
+                <input type="number" placeholder="Minutes" onChange={onMinutesChange} value={minutes}/>
+                <input type="number" placeholder="Hours" onChange={onHoursChange} value={hours}/>
             </div>
         </>
     );
