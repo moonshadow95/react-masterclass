9ceb71fbf460176857e309bfd427b084ec417797
ictus <ictus@ictusui-MacBookPro.local>
2022-03-04 11:38:15 +0900

    Categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {IToDo} from "../atoms";
 
-const ToDo = ({text}: IToDo) => {
+const ToDo = ({id, text, category}: IToDo) => {
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const {currentTarget: {name}} = event
+    }
     return (
         <li>
             <span>{text}</span>
-            <button>ToDo</button>
-            <button>Doing</button>
-            <button>DONE</button>
+            {category !== "TO_DO" && <button name="TO_DO" onClick={onClick}>ToDo</button>}
+            {category !== "DOING" && <button name="DOING" onClick={onClick}>Doing</button>}
+            {category !== "DONE" && <button name="DONE" onClick={onClick}>DONE</button>}
         </li>
 
     );
