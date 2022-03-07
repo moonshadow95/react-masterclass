c96f10f59bccfcb124226ac985b870881904dce1
ictus <ictus@ictusui-MacBookPro.local>
2022-03-07 11:23:08 +0900

    Drag and Drop - Beautiful-dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,76 @@
-import React, {useState} from 'react';
-import {createGlobalStyle} from "styled-components";
+import React from 'react';
+import {DragDropContext, Draggable, Droppable, DropResult} from "react-beautiful-dnd";
+import styled from "styled-components";
 import {useRecoilState, useRecoilValue} from "recoil";
-import {hourSelector, minuteState} from "./atoms";
+import {toDoState} from "./atoms";
 
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500;700&family=Noto+Sans:wght@700&display=swap');
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
 
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-  }
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+`
 
-  /* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, menu, nav, section {
-    display: block;
-  }
+const Board = styled.div`
+  padding: 20px 10px;
+  border-radius: 5px;
+  background-color: ${props => props.theme.boardColor};
+`
 
-  body {
-    line-height: 1;
-    font-family: 'Noto Sans KR', sans-serif;
-
-    ol, ul {
-      list-style: none;
-    }
-
-    blockquote, q {
-      quotes: none;
-    }
-
-    blockquote:before, blockquote:after,
-    q:before, q:after {
-      content: '';
-      content: none;
-    }
-
-    * {
-      box-sizing: border-box;
-    }
-
-    table {
-      border-collapse: collapse;
-      border-spacing: 0;
-    }
-
-    a {
-      text-decoration: none;
-      color: inherit;
-    }
-  }
+const Card = styled.div`
+  background-color: ${props => props.theme.cardColor};
+  padding: 5px;
+  border-radius: 5px;
+  margin-bottom: 5px;
 `
 
 function App() {
-    const [minutes, setMinutes] = useRecoilState(minuteState)
-    const [hours, setHours] = useRecoilState(hourSelector)
-    const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-        setMinutes(+event.currentTarget.value)
-    }
-    const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
-        setHours(+event.currentTarget.value)
+    const [toDos, setToDos] = useRecoilState(toDoState)
+    // destination 목적지, source 움직인 아이템
+    // source를 삭제하고 destination 위치에 넣는다.
+    const onDragEnd = ({draggableId, destination, source}: DropResult) => {
+        if (!destination) return
+        setToDos(oldToDos => {
+                const toDosCopy = [...oldToDos]
+                // 1) Delete item on source.index
+                toDosCopy.splice(source.index, 1)
+                // 2) Put it back on the destination.index
+                toDosCopy.splice(destination?.index, 0, draggableId)
+                return toDosCopy
+            }
+        )
     }
     return (
         <>
-            <GlobalStyle/>
-            <div>
-                <input type="number" placeholder="Minutes" onChange={onMinutesChange} value={minutes}/>
-                <input type="number" placeholder="Hours" onChange={onHoursChange} value={hours}/>
-            </div>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Wrapper>
+                    <Boards>
+                        <Droppable droppableId='1'>
+                            {(provided) =>
+                                <Board {...provided.droppableProps} ref={provided.innerRef}>
+                                    {toDos.map((toDo, index) => <Draggable key={toDo} draggableId={toDo} index={index}>
+                                        {(provided) =>
+                                            <Card {...provided.dragHandleProps} {...provided.draggableProps}
+                                                  ref={provided.innerRef}>{toDo}</Card>}
+                                    </Draggable>)}
+                                    {provided.placeholder}
+                                </Board>
+                            }
+                        </Droppable>
+                    </Boards>
+                </Wrapper>
+            </DragDropContext>
         </>
-    );
+    )
+        ;
 }
 
 export default App;
